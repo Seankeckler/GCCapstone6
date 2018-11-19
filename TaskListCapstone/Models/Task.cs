2c53f2d8febc931013b40d3dded1708e34c18732
Sean Keckler <seankeckler@gmail.com>
2018-11-19 08:38:08 -0500

    Initial Commit

---
+++ TaskListCapstone/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaskListCapstone.Models
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public int UserID { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public bool Complete { get; set; }
+    }
+
+    public class DbTaskContext : DbContext
+    {
+        //DbSet<Task> Tasks { get; set; }
+
+       public System.Data.Entity.DbSet<TaskListCapstone.Models.Task> Tasks { get; set; }
+    }
+}
---
+++ TaskListCapstone/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaskListCapstone.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class DbUserContext : DbContext
+    {
+        //DbSet<User> Users { get; set; }
+
+       public System.Data.Entity.DbSet<TaskListCapstone.Models.User> Users { get; set; }
+    }
+}
